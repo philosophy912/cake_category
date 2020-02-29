d7ece2c92c24bac8a8807f357ff8621b2b416e39
lizhe <totti_912@sina.com>
2020-02-29 23:02:00 +0800

    修改了materialDao的错误

--- src/main/java/com/sophia/cake/dao/MaterialProductDao.java
+++ src/main/java/com/sophia/cake/dao/MaterialProductDao.java
@@ -1,10 +1,11 @@
 package com.sophia.cake.dao;
 
+import com.sophia.cake.entity.MaterialProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @author lizhe
  * @date 2020-02-25 11:29
  */
-public interface MaterialProductDao extends JpaRepository<MaterialProductDao, Integer> {
+public interface MaterialProductDao extends JpaRepository<MaterialProduct, Integer> {
 }
