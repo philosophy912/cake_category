7b8f0fd4a11bc6fcb9f8e975d7cfe2224613f14e
philosophy <totti_912@sina.com>
2020-03-26 15:26:07 +0800

    run已经OK了

--- cake/src/main/java/com/sophia/cake/controller/MaterialController.java
+++ cake/src/main/java/com/sophia/cake/controller/MaterialController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
